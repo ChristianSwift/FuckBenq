331ec2dd5c38948830a59443e0d58f282eb02a30
ChristianSwift <topman@dingnaiwen.xyz>
2019-03-24 20:49:31 +0800

    Functional

--- ProjectFuckBenQ/ProjectFuckBenQ.ino
+++ ProjectFuckBenQ/ProjectFuckBenQ.ino
@@ -1,12 +1,40 @@
 #include <Encoder.h>
 
+long oldPosition = 0;
+long newPosition = 0;
+
 Encoder myEnc(D2, D3);
+const int FunctionStatePin = D1;
+int ColorTemperature = 5;
+int LightStrength = 5;
 
 void setup() {
+  pinMode(FunctionStatePin, INPUT);
   Serial.begin(115200);
   Serial.println("Basic Encoder Test:");
 }
 
 void loop() {
-  Serial.println(myEnc.read()/4);
+  long newPosition = myEnc.read()/4;
+  if (newPosition > oldPosition) {
+    oldPosition = newPosition;
+    if (digitalRead(FunctionStatePin) == HIGH) {
+      ColorTemperature++;
+    } else {
+      LightStrength++;
+    }
+    Serial.print(ColorTemperature);
+    Serial.print("|");
+    Serial.println(LightStrength);
+  }else if (newPosition < oldPosition) {
+    oldPosition = newPosition;
+    if (digitalRead(FunctionStatePin) == HIGH) {
+      ColorTemperature--;
+    } else {
+      LightStrength--;
+    }
+    Serial.print(ColorTemperature);
+    Serial.print("|");
+    Serial.println(LightStrength);
+  }
 }
