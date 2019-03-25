ec4a171082dc137e77fe10e13dc6a235cdb5887f
ChristianSwift <topman@dingnaiwen.xyz>
2019-03-25 14:02:30 +0800

    Fix Standard Illumination to HEX Code

--- ProjectFuckBenQ/ProjectFuckBenQ.ino
+++ ProjectFuckBenQ/ProjectFuckBenQ.ino
@@ -3,41 +3,16 @@
 long oldPosition = 0;
 long newPosition = 0;
 
-char hexdata[4]={0x00,0x00,0x00,0x00};
-
 Encoder myEnc(D2, D3);
 const int FunctionStatePin = D1;
 int ColorTemperature = 5;
 int LightStrength = 5;
 
 void LightDataOutput(int data1,int data2){
-  //Serial.print("冷光绝对亮度:");
-  //Serial.print(data1*data2);
-  //Serial.print(" | 暖光绝对亮度:");
-  //Serial.println(data1*(10-data2));
-  
   int coldbrightness=data1*data2;
   int warmbrightness=data1*(10-data2);
-
   Serial.write(31);Serial.write(01);Serial.write(coldbrightness);Serial.write(31+01+coldbrightness);
   Serial.write(31);Serial.write(02);Serial.write(warmbrightness);Serial.write(31+02+warmbrightness);
-//  Serial.print("冷光:");
-//  Serial.print(31,HEX);
-//  Serial.print(" ");
-//  Serial.print(01,HEX);
-//  Serial.print(" ");
-//  Serial.print(coldbrightness,HEX);
-//  Serial.print(" ");
-//  Serial.println(31+01+coldbrightness,HEX);
-//  
-//  Serial.print("暖光:");
-//  Serial.print(31,HEX);
-//  Serial.print(" ");
-//  Serial.print(02,HEX);
-//  Serial.print(" ");
-//  Serial.print(warmbrightness,HEX);
-//  Serial.print(" ");
-//  Serial.println(31+01+warmbrightness,HEX);
 }
 
 void setup() {
