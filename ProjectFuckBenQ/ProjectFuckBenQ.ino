1825e530ef48457af3f2ad365642a33df9aa4d86
ChristianSwift <topman@dingnaiwen.xyz>
2019-03-25 10:59:47 +0800

    Fix Standard Illumination

--- ProjectFuckBenQ/ProjectFuckBenQ.ino
+++ ProjectFuckBenQ/ProjectFuckBenQ.ino
@@ -3,15 +3,25 @@
 long oldPosition = 0;
 long newPosition = 0;
 
+unsighed char hexdata[4]={0x1F,0x01,0x00,0x20};
+
 Encoder myEnc(D2, D3);
 const int FunctionStatePin = D1;
 int ColorTemperature = 5;
 int LightStrength = 5;
 
+void LightDataOutput(int data1,int data2){
+  Serial.print("冷光绝对亮度:");
+  Serial.print(data1*data2);
+  Serial.print(" | 暖光绝对亮度:");
+  Serial.println(data1*(10-data2));
+}
+
 void setup() {
   pinMode(FunctionStatePin, INPUT_PULLUP);
   Serial.begin(115200);
   Serial.println("Basic Encoder Test:");
+  LightDataOutput(LightStrength,ColorTemperature);
 }
 
 void loop() {
@@ -27,10 +37,7 @@ void loop() {
         ColorTemperature++;
       }
     }
-    Serial.print("亮度:");
-    Serial.print(LightStrength);
-    Serial.print(" | 混光:");
-    Serial.println(ColorTemperature);
+    LightDataOutput(LightStrength,ColorTemperature);
   }else if (newPosition < oldPosition) {
     oldPosition = newPosition;
     if (digitalRead(FunctionStatePin) == HIGH) {
@@ -42,9 +49,6 @@ void loop() {
         ColorTemperature--;
       }
     }
-    Serial.print("亮度:");
-    Serial.print(LightStrength);
-    Serial.print(" | 混光:");
-    Serial.println(ColorTemperature);
+    LightDataOutput(LightStrength,ColorTemperature);
   }
 }
