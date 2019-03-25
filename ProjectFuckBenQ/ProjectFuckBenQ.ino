bb14b88781cf51e2e7f9a2786c1c5e1d911f0cdf
ChristianSwift <topman@dingnaiwen.xyz>
2019-03-25 18:45:40 +0800

    Added display function

--- ProjectFuckBenQ/ProjectFuckBenQ.ino
+++ ProjectFuckBenQ/ProjectFuckBenQ.ino
@@ -1,5 +1,8 @@
 #include <Encoder.h>
 #include <EasyButton.h>
+#include <oled.h>
+
+OLED display=OLED(4,5,16);
 
 const int FunctionStatePin = D7;
 const int BoostButtonPin = D6;
@@ -14,12 +17,32 @@ long newPosition = 0;
 Encoder myEnc(D2, D3);
 int ColorTemperature = 5;
 int LightStrength = 5;
+char displayLightStrength[10];
+char displayColorTemperature[10];
 
 void LightDataOutput(int data1,int data2) {
+  display.clear();
   int coldbrightness=data1*data2;
   int warmbrightness=data1*(10-data2);
   Serial.write(31);Serial.write(01);Serial.write(coldbrightness);Serial.write(31+01+coldbrightness);
   Serial.write(31);Serial.write(02);Serial.write(warmbrightness);Serial.write(31+02+warmbrightness);
+  if(data1==0){
+    itoa(data1*10,displayLightStrength,10);
+    display.draw_string_P(0, 0,"Brht:    %",OLED::DOUBLE_SIZE);
+    display.draw_string_P(96, 0,displayLightStrength,OLED::DOUBLE_SIZE);
+  }else if(data1==10||data1==20){
+    itoa(data1*10,displayLightStrength,10);
+    display.draw_string_P(0, 0,"Brht:    %",OLED::DOUBLE_SIZE);
+    display.draw_string_P(72, 0,displayLightStrength,OLED::DOUBLE_SIZE);
+  }else{
+    itoa(data1*10,displayLightStrength,10);
+    display.draw_string_P(0, 0,"Brht:    %",OLED::DOUBLE_SIZE);
+    display.draw_string_P(84, 0,displayLightStrength,OLED::DOUBLE_SIZE);
+  }
+  itoa(data2*380+2700,displayColorTemperature,10);
+  display.draw_string_P(0,16,"Temp:    K",OLED::DOUBLE_SIZE);
+  display.draw_string_P(60,16,displayColorTemperature,OLED::DOUBLE_SIZE);
+  display.display();
 }
 
 void ButtonEventSingle() {
@@ -48,6 +71,7 @@ void setup() {
   ButtonBoost.begin();
   ButtonBoost.onPressed(ButtonEventSingle);
   ButtonBoost.onPressedFor(1500, ButtonEventLong);
+  display.begin(); 
   LightDataOutput(LightStrength,ColorTemperature);
 }
 
