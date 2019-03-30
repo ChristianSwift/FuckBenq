2dda3ef7d6bea90ee9a3798cfec61ccddac72adc
ChristianSwift <topman@dingnaiwen.xyz>
2019-03-30 15:15:13 +0800

    Slash to D1 mini

--- ProjectFuckBenQ/ProjectFuckBenQ.ino
+++ ProjectFuckBenQ/ProjectFuckBenQ.ino
@@ -1,48 +1,27 @@
 #include <Encoder.h>
 #include <EasyButton.h>
-#include <oled.h>
 
-OLED display=OLED(4,5,16);
+long oldPosition = 0;
+long newPosition = 0;
 
-const int FunctionStatePin = D7;
-const int BoostButtonPin = D6;
+Encoder myEnc(D1, D2);
+const int FunctionStatePin = D3;
+const int BoostButtonPin = D5;
+const int WarmLightStrip = D7;
+const int ColdLightStrip = D6;
+int ColorTemperature = 5;
+int LightStrength = 5;
 
 EasyButton ButtonBoost(BoostButtonPin);
 bool BootState = true;
 bool BoostState = false;
 
-long oldPosition = 0;
-long newPosition = 0;
-
-Encoder myEnc(D2, D3);
-int ColorTemperature = 5;
-int LightStrength = 5;
-char displayLightStrength[10];
-char displayColorTemperature[10];
-
-void LightDataOutput(int data1,int data2) {
-  display.clear();
-  int coldbrightness=data1*data2;
-  int warmbrightness=data1*(10-data2);
-  Serial.write(31);Serial.write(01);Serial.write(coldbrightness);Serial.write(31+01+coldbrightness);
-  Serial.write(31);Serial.write(02);Serial.write(warmbrightness);Serial.write(31+02+warmbrightness);
-  if(data1==0){
-    itoa(data1*10,displayLightStrength,10);
-    display.draw_string_P(0, 0,"Brht:    %",OLED::DOUBLE_SIZE);
-    display.draw_string_P(96, 0,displayLightStrength,OLED::DOUBLE_SIZE);
-  }else if(data1==10||data1==20){
-    itoa(data1*10,displayLightStrength,10);
-    display.draw_string_P(0, 0,"Brht:    %",OLED::DOUBLE_SIZE);
-    display.draw_string_P(72, 0,displayLightStrength,OLED::DOUBLE_SIZE);
-  }else{
-    itoa(data1*10,displayLightStrength,10);
-    display.draw_string_P(0, 0,"Brht:    %",OLED::DOUBLE_SIZE);
-    display.draw_string_P(84, 0,displayLightStrength,OLED::DOUBLE_SIZE);
-  }
-  itoa(data2*380+2700,displayColorTemperature,10);
-  display.draw_string_P(0,16,"Temp:    K",OLED::DOUBLE_SIZE);
-  display.draw_string_P(60,16,displayColorTemperature,OLED::DOUBLE_SIZE);
-  display.display();
+void LightDataOutput(int data1,int data2){
+  int coldbrightness=data1*data2*10;
+  int warmbrightness=data1*(10-data2)*10;
+  Serial.print(coldbrightness);Serial.print("|");Serial.println(warmbrightness);
+  analogWrite(ColdLightStrip, coldbrightness);
+  analogWrite(WarmLightStrip, warmbrightness);
 }
 
 void ButtonEventSingle() {
@@ -51,6 +30,7 @@ void ButtonEventSingle() {
     BootState=true;
   }else if (BootState==true) {
     LightDataOutput(0,5);
+    BoostState=false;
     BootState=false;
   }
 }
@@ -58,6 +38,7 @@ void ButtonEventSingle() {
 void ButtonEventLong() {
   if (BoostState==false){
     LightDataOutput(20,5);
+    BootState=true;
     BoostState=true;
   }else if (BoostState==true) {
     LightDataOutput(LightStrength,ColorTemperature);
@@ -70,8 +51,7 @@ void setup() {
   Serial.begin(115200);
   ButtonBoost.begin();
   ButtonBoost.onPressed(ButtonEventSingle);
-  ButtonBoost.onPressedFor(1500, ButtonEventLong);
-  display.begin(); 
+  ButtonBoost.onPressedFor(800, ButtonEventLong);
   LightDataOutput(LightStrength,ColorTemperature);
 }
 
@@ -89,6 +69,8 @@ void loop() {
       }
     }
     LightDataOutput(LightStrength,ColorTemperature);
+    BootState=true;
+    BoostState=false;
   }else if (newPosition < oldPosition) {
     oldPosition = newPosition;
     if (digitalRead(FunctionStatePin) == HIGH) {
@@ -101,6 +83,8 @@ void loop() {
       }
     }
     LightDataOutput(LightStrength,ColorTemperature);
+    BootState=true;
+    BoostState=false;
   }
   ButtonBoost.read();
-}
+}      
